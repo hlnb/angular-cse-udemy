929b052442120066692bd75b65720f39c36d14ce
Elizabeth H Burgess <heb264@gmail.com>
2017-12-10 19:48:07 +0800

    service component

---
+++ udemy-first-app/src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+//decorator
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+//class
+export class ServerComponent {
+
+}
