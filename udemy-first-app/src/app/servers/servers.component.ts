190cd9434388fe31365dbea916dcc7b91cde1239
Elizabeth H Burgess <heb264@gmail.com>
2017-12-10 20:12:22 +0800

    finished first section

--- udemy-first-app/src/app/servers/servers.component.ts
+++ udemy-first-app/src/app/servers/servers.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  selector: 'app-servers', 
+  //selector: '[app-servers]' as an attribute
+ // selector: '.app-servers' as a class
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>
+  `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
